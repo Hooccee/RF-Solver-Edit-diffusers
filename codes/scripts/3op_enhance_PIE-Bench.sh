b55941c237cea44cf9e572a81e51e912ad5481ad
HocChueng <1401698558@qq.com>
2025-03-18 20:50:55 +0800

    fix(scripts): 修正 3op_enhance 脚本中的参数拼写错误

--- codes/scripts/3op_enhance_PIE-Bench.sh
+++ codes/scripts/3op_enhance_PIE-Bench.sh
@@ -11,5 +11,5 @@ python3  main_3op_enhance.py \
     --ilist_json_path ../ilist_data_PIE-Bench.json \
     --output_dir ./outputs/3op_enhance \
     --test \
-    --enchanced \ 
+    --enhanced \ 
     --stable_flow
