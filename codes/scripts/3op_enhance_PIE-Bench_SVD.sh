a77af59b78f0081cc2ce231a3ad8ec7f5067ccb2
HocChueng <1401698558@qq.com>
2025-04-08 22:49:26 +0800

    fix(script): 更新可调参数注释以包含参考范围和说明

--- codes/scripts/3op_enhance_PIE-Bench_SVD.sh
+++ codes/scripts/3op_enhance_PIE-Bench_SVD.sh
@@ -21,15 +21,15 @@ python3  main_3op_enhance_39_SVD.py \
     --v 3     \
 
     #可调参数
-    # parser.add_argument('--nudge', type=float, default=1.05, help='target inv分支潜变量标量偏移')
-    # parser.add_argument('--guidance_scale', type=float, default=3.5, help='denoise 伪cfg 的引导比例')
-    # parser.add_argument('--inject', type=int, default=8,help='inject步数')
-    # parser.add_argument('--nudge_source', type=float, default=1.15, help='source inv分支潜变量标量偏移')
-    # parser.add_argument('--K', type=int, default=5, help='SVD TopK 基数')
-    # parser.add_argument('--alpha', type=float, default=3, help='SVD 增强 注意力权重放大系数')
-    # parser.add_argument('--v: ', type=float, default=3, help='SVD 增强 Sigmoid斜率系数')
-    # parser.add_argument('--SVD_start: ', type=float, default=0.96, help='SVD 增强 开始时间(denoise t 从1.0到0.0)')
-    # parser.add_argument('--SVD_end: ', type=float, default=0.85, help='SVD 增强 结束时间(denoise t 从1.0到0.0)')
+    # parser.add_argument('--nudge', type=float, default=1.05, help='target inv分支潜变量标量偏移')参考范围：1.05~1.15，太大会使画质劣化
+    # parser.add_argument('--guidance_scale', type=float, default=3.5, help='denoise 伪cfg 的引导比例') 参考范围：2.5~5.5，越小与原图越相似
+    # parser.add_argument('--inject', type=int, default=8,help='inject步数') 参考范围：15~11，越大与原图越相似
+    # parser.add_argument('--nudge_source', type=float, default=1.15, help='source inv分支潜变量标量偏移') 参考范围：1.05~1.15，越大与原图越相似
+    # parser.add_argument('--K', type=int, default=5, help='SVD TopK 基数') 参考范围：2~7，越小与原图越相似
+    # parser.add_argument('--alpha', type=float, default=3, help='SVD 增强 注意力权重放大系数') 参考范围：2~7，越小与原图越相似
+    # parser.add_argument('--v: ', type=float, default=3, help='SVD 增强 Sigmoid斜率系数') 参考范围：2~7，越小与原图越相似
+    # parser.add_argument('--SVD_start: ', type=float, default=0.96, help='SVD 增强 开始时间(denoise t 从1.0到0.0)') 不能太靠近1，又不能太远离1
+    # parser.add_argument('--SVD_end: ', type=float, default=0.85, help='SVD 增强 结束时间(denoise t 从1.0到0.0)') 越小SVD增强越明显
 
 
 
